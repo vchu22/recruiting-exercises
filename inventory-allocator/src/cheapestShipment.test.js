a6ccf7577878ad958adf0c5d034f1d84a1674396
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2020-08-30 19:41:12 -0400

    update test specs

--- inventory-allocator/src/cheapestShipment.test.js
+++ inventory-allocator/src/cheapestShipment.test.js
@@ -6,28 +6,41 @@ test("Function returns an array", () => {
 
 test("Order can be shipped using one warehouse", () => {
   const items = { apple: 1 };
-  const warehouses = [{ name: owd, inventory: { apple: 1 } }];
+  const warehouses = [{ name: "owd", inventory: { apple: 1 } }];
   const answer = [{ owd: { apple: 1 } }];
-  expect(cheapestShipment(items, warehouses)).toBe(answer);
+  expect(cheapestShipment(items, warehouses)).toEqual(answer);
 });
 
 test("Order can be shipped using multiple warehouses", () => {
   const items = { apple: 10 };
   const warehouses = [
-    { name: owd, inventory: { apple: 5 } },
-    { name: dm, inventory: { apple: 5 } },
+    { name: "owd", inventory: { apple: 5 } },
+    { name: "dm", inventory: { apple: 5 } },
   ];
   const answer = [{ dm: { apple: 5 } }, { owd: { apple: 5 } }];
-  expect(cheapestShipment(items, warehouses)).toBe(answer);
+  expect(cheapestShipment(items, warehouses)).toEqual(answer);
 });
 
 test("Order cannot be shipped because there is not enough inventory", () => {
-  const items = { apple: 1 };
-  const warehouses = [{ name: owd, inventory: { apple: 0 } }];
+  let items = { apple: 1 };
+  let warehouses = [{ name: "owd", inventory: { apple: 0 } }];
   const answer = [];
-  expect(cheapestShipment(items, warehouses)).toBe(answer);
+  expect(cheapestShipment(items, warehouses)).toEqual(answer);
 
   items = { apple: 2 };
-  warehouses = [{ name: owd, inventory: { apple: 1 } }];
-  expect(cheapestShipment(items, warehouses)).toBe(answer);
+  warehouses = [{ name: "owd", inventory: { apple: 1 } }];
+  expect(cheapestShipment(items, warehouses)).toEqual(answer);
+});
+
+test("Multiple items can be shipped using multiple warehouses", () => {
+  const items = { apple: 5, banana: 5, orange: 5 };
+  const warehouses = [
+    { name: "owd", inventory: { apple: 5, orange: 10 } },
+    { name: "dm", inventory: { banana: 5, orange: 10 } },
+  ];
+  const answer = [
+    { owd: { apple: 5, orange: 5 } },
+    { dm: { banana: 5, orange: 5 } },
+  ];
+  expect(cheapestShipment(items, warehouses)).toEqual(answer);
 });
