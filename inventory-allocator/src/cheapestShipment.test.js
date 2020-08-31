7a1b6de35b911440098f1f6dc1d9c5d49690ce86
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2020-08-30 22:30:40 -0400

    updated test specs

--- inventory-allocator/src/cheapestShipment.test.js
+++ inventory-allocator/src/cheapestShipment.test.js
@@ -1,39 +1,39 @@
 const cheapestShipment = require("./cheapestShipment");
 
 test("Function returns an array", () => {
-  expect(Array.isArray(cheapestShipment())).toBe(true);
+  expect(Array.isArray(cheapestShipment({}, []))).toBe(true);
 });
 
 test("Order can be shipped using one warehouse", () => {
-  const items = { apple: 1 };
+  const shipments = { apple: 1 };
   const warehouses = [{ name: "owd", inventory: { apple: 1 } }];
   const answer = [{ owd: { apple: 1 } }];
-  expect(cheapestShipment(items, warehouses)).toEqual(answer);
+  expect(cheapestShipment(shipments, warehouses)).toEqual(answer);
 });
 
 test("Order can be shipped using multiple warehouses", () => {
-  const items = { apple: 10 };
+  const shipments = { apple: 10 };
   const warehouses = [
     { name: "owd", inventory: { apple: 5 } },
     { name: "dm", inventory: { apple: 5 } },
   ];
-  const answer = [{ dm: { apple: 5 } }, { owd: { apple: 5 } }];
-  expect(cheapestShipment(items, warehouses)).toEqual(answer);
+  const answer = [{ owd: { apple: 5 } }, { dm: { apple: 5 } }];
+  expect(cheapestShipment(shipments, warehouses)).toEqual(answer);
 });
 
 test("Order cannot be shipped because there is not enough inventory", () => {
-  let items = { apple: 1 };
+  let shipments = { apple: 1 };
   let warehouses = [{ name: "owd", inventory: { apple: 0 } }];
   const answer = [];
-  expect(cheapestShipment(items, warehouses)).toEqual(answer);
+  expect(cheapestShipment(shipments, warehouses)).toEqual(answer);
 
-  items = { apple: 2 };
+  shipments = { apple: 2 };
   warehouses = [{ name: "owd", inventory: { apple: 1 } }];
-  expect(cheapestShipment(items, warehouses)).toEqual(answer);
+  expect(cheapestShipment(shipments, warehouses)).toEqual(answer);
 });
 
-test("Multiple items can be shipped using multiple warehouses", () => {
-  const items = { apple: 5, banana: 5, orange: 5 };
+test("Multiple shipments can be shipped using multiple warehouses", () => {
+  const shipments = { apple: 5, banana: 5, orange: 5 };
   const warehouses = [
     { name: "owd", inventory: { apple: 5, orange: 10 } },
     { name: "dm", inventory: { banana: 5, orange: 10 } },
@@ -42,5 +42,5 @@ test("Multiple items can be shipped using multiple warehouses", () => {
     { owd: { apple: 5, orange: 5 } },
     { dm: { banana: 5, orange: 5 } },
   ];
-  expect(cheapestShipment(items, warehouses)).toEqual(answer);
+  expect(cheapestShipment(shipments, warehouses)).toEqual(answer);
 });
